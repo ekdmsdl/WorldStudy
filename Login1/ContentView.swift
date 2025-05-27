0bc8faa4e8ced54379613d60a031b5c6b7a325f0
이다은 <jegalmin02@gmail.com>
2025-05-28 00:02:07 +0900

    Initial Commit

---
+++ Login1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Login1
+//
+//  Created by 이다은 on 5/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Login1/Login1App.swift
@@ -0,0 +1,17 @@
+//
+//  Login1App.swift
+//  Login1
+//
+//  Created by 이다은 on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Login1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
